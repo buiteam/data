2abe8327a3d5f98b267a5055fd97e65d50b430da
Diluka <pmars42@gmail.com>
2015-04-07 11:10:49 +0800

    Update treestore.js

--- src/treestore.js
+++ src/treestore.js
@@ -574,9 +574,7 @@
       if(pidField){
         //params[pidField] = node.id;
         //pid应该是node.parent.id而不是node.id
-        params[pidField] = node.parent ? node.parent.id : 
-          node.pid ? node.pid : 
-          node.id ? node.id : 0;
+        params[pidField] = node.parent ? node.parent.id : node.pid || node.id || 0;
       }
       _self.load(params);  
     },
